8c4a92e841c120c0fcc48950087aa75264274b80
Mara6336-T <tamarapetrik4@gmail.com>
2026-03-02 11:40:46 +0100

    hibák kikommentelése a main.cpp kódban

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //hibás elnevezés N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //pontosvessző hiánya, idézőjel helyetti hasnálata : '
+    for (int i = 0;)//hiányos feltétel, inkreventálás
     {
-        b[i] = i * 2;
+        b[i] = i * 2;//rossz képlet használata (i + 1) * 2
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)//hibás feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"//nincs pontosvessző
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;//kezdőérték hiányzik
+    for (int i = 0; i < N_ELEMENTS, i++)//hibás ciklus meghatározás
     {
-        atlag += b[i]
+        atlag += b[i]//pontosvessző hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
+    //nincs delete-->memóriaszivárgás
 }
